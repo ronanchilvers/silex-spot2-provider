5ba8fa7f2907d1d6c2b01a44fc4016b17df28eff
Ronan Chilvers <ronan@d3r.com>
2016-01-11 21:43:46 +0000

    Add stub file for provider

---
+++ src/Silex/Provider/Spot2ServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ronanchilvers\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class Spot2ServiceProvider implements ServiceProviderInterface
+{
+
+    public function register(Application $app)
+    {
+
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
+
